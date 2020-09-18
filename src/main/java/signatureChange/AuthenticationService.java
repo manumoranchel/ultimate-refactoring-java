279449c031d7d35fb5f36009385cbae5197a4b20
Manuel Moranchel <manuel.moranchel@mindcurv.com>
2020-09-18 12:23:35 +0200

    Using the new class Id and the right method with such POJO

--- src/main/java/signatureChange/AuthenticationService.java
+++ src/main/java/signatureChange/AuthenticationService.java
@@ -2,18 +2,9 @@ package signatureChange;
 
 
 public class AuthenticationService {
-    public boolean isAuthenticated(int id) {
-        Id user = new Id(id);
-        return user.isValidUser();
+
+    public boolean isAuthenticated(Id id) {
+        return id.isValidUser();
     }
 
-    public boolean isAdminAuthenticated(int id) {
-        Id user = new Id(id);
-        return user.isAdmin();
-    }
-
-    public boolean isUserAuthenticated(int id) {
-        Id user = new Id(id);
-        return user.isUser();
-    }
 }
