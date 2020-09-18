8482a94a08ab4cb7492e34b2e53a44acd04b9761
Manuel Moranchel <manuel.moranchel@mindcurv.com>
2020-09-18 11:54:44 +0200

    adding User profile to User class

--- src/main/java/signatureChange/User.java
+++ src/main/java/signatureChange/User.java
@@ -2,6 +2,7 @@ package signatureChange;
 
 public class User {
     static int ADMIN_ID = 12345;
+    static int USER_ID = 11111;
     public int id;
 
     public User(int id) {
@@ -11,4 +12,9 @@ public class User {
     public boolean isAdmin() {
         return ADMIN_ID == this.id;
     }
+
+    public boolean isUser() {
+        return USER_ID == this.id;
+    }
+
 }
